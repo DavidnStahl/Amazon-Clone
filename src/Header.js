a7f323ad7ba2478a62560a46e10fede900fdf4b9
David Stahl <david.n.stahl@gmail.com>
2020-10-26 16:47:03 +0100

    header component styled and looking nice

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 export default function Header() {
     return (
@@ -15,13 +16,15 @@ export default function Header() {
                 <input
                 className="header__searchInput"
                 type="text"/>
-                <SearchIcon/>
+                <SearchIcon
+                className="header__searchIcon"
+                />
             </div>
 
             <div className="header__nav">
               <div className="header__option">
                  <span className="header__optionLineOne">
-                 Hello guest</span>
+                 Hello Guest</span>
                  <span className="header__optionLineTwo">
                  Sign In</span>
               </div>
@@ -38,6 +41,11 @@ export default function Header() {
                  Your</span>
                  <span className="header__optionLineTwo">
                  Prime</span>
+              </div>
+              <div className="header__optionBasket">
+                <ShoppingBasketIcon/>
+                <span className="header__optionLineTwo
+                header__basketCount">0</span>
               </div>
             </div>
             

7b22a220cdf840222dd4487195f65fa86fddfc2c
David Stahl <david.n.stahl@gmail.com>
2020-10-26 16:19:54 +0100

    material-ui installed and fundation for header component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB7sBcjV6y5j9qO3X1Fuic88TBBAyJ2Gqw",
+  authDomain: "clone-98742.firebaseapp.com",
+  databaseURL: "https://clone-98742.firebaseio.com",
+  projectId: "clone-98742",
+  storageBucket: "clone-98742.appspot.com",
+  messagingSenderId: "680448171585",
+  appId: "1:680448171585:web:1859a4e3f54a4156c3f0c0"
+};
